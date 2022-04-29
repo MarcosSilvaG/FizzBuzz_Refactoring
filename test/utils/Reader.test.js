3a81ccfb11b208380811d2c31f8e841de5e02588
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-29 10:20:11 -0500

    Adding tests for Reader file

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,5 +1,13 @@
 const Reader = require('../../lib/utils/Reader')
 
 describe("Unit tests for Reader file", () => {
-    const explorer = Reader.readJsonFile()
+    test("1. Return data from json file", () => {
+        const explorer = Reader.readJsonFile("test/data/explorers_1.json")
+        const explorerName = explorer.map(explorerName1 => explorerName1.name)
+        expect(explorerName).toContain('Mingo')
+    })
+    test("2. Get total number of explorers", () => {
+        const explorers = Reader.readJsonFile("test/data/explorers_1.json")       
+        expect(explorers.length).toBe(16)
+    })
 })
