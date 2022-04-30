97939254b9ee1d870360d73dadd7ad2dab970880
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-30 15:35:47 -0500

    Adding the methods getExplorersUsernamesByMission and getExplorersAmountByMission to the ExplorerController file and its respective tests

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -5,4 +5,24 @@ describe("Unit Tests for explorer controller file", () => {
         const explorersByMission = ExplorerController.getExplorerByMission("node");
         expect(explorersByMission).not.toBeNull();        
     });
+    test("2) Test for getExplorerByMission length", () => {
+        const explorerByMissionLength = ExplorerController.getExplorerByMission("node");
+        expect(explorerByMissionLength).toHaveLength(10);
+    });
+    test("3) Test for getExplorersUsernamesByMission method", () => {
+        const explorersUsernamesByMission = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorersUsernamesByMission).toContain("ajolonauta1");
+    });
+    test("4) Test for getExplorersUsernamesByMission, not to be null", () => {
+        const explorersUsernamesByMission = ExplorerController.getExplorersUsernamesByMission("java");
+        expect(explorersUsernamesByMission).not.toBeNull();
+    });
+    test("5) Test for getExplorerAmountByMission method", () => {
+        const explorersAmountByMissions = ExplorerController.getExplorersAmountByMission("java");
+        expect(explorersAmountByMissions).toBe(5);
+    });
+    test("6) Test for getExplorerAmountByMission length", () => {
+        const explorersAmountByMissions = ExplorerController.getExplorersAmountByMission("node");
+        expect(explorersAmountByMissions).toBe(explorersAmountByMissions);
+    });
 });
