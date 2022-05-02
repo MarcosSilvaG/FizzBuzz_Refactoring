1f00dc744bafa3e7e975e51ed971844c87338cb6
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-05-02 13:57:34 -0500

    Adding tests for new service:  applyValidationInNumber()

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -25,4 +25,25 @@ describe("Unit Tests for explorer controller file", () => {
         const explorersAmountByMissions = ExplorerController.getExplorersAmountByMission("node");
         expect(explorersAmountByMissions).toBe(explorersAmountByMissions);
     });
+    test("7) Test for getFizzBuzzService controller equal to 5", () => {
+        const number5 = ExplorerController.getFizzBuzzService(5);
+        expect(number5).toBe("BUZZ");
+    });
+    test("7) Test for getFizzBuzzService controller equal to 3", () => {
+        const number3 = ExplorerController.getFizzBuzzService(3);
+        expect(number3).toBe("FIZZ");
+    });
+    test("8) Test for getFizzBuzzService controller qual to 15", () => {
+        const number15 = ExplorerController.getFizzBuzzService(15);
+        expect(number15).toBe("FIZZBUZZ");
+    });
+    test("7) Test for new service applyValidationInNumber with number not divisible by 3 or 5", () => {
+        const number1 = ExplorerController.getFizzBuzzService(1);
+        const number2 = ExplorerController.getFizzBuzzService(2);
+        const number7 = ExplorerController.getFizzBuzzService(7);
+        
+        expect(number1).toBe(1);
+        expect(number2).toBe(2);
+        expect(number7).toBe(7);
+    });
 });
