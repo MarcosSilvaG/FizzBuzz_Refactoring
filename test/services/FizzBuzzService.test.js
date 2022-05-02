ca98a02a7cec981f88020eba47c8d0f5bc98bf36
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-05-02 13:32:31 -0500

    Adding code of the new service: applyValidationInNumber() and its respective tests

--- test/services/FizzBuzzService.test.js
+++ test/services/FizzBuzzService.test.js
@@ -21,4 +21,20 @@ describe("Tests for FizBuzz class", () => {
         const explorerFizzBuzz = FizzBuzzService.applyValidationInExplorer(explorer15);
         expect(explorerFizzBuzz.trick).toBe("FIZZBUZZ");
     });
+    test("5) Test for new service applyValidationInNumber equal to 15", () => {
+        const number15 = FizzBuzzService.applyValidationInNumber(15);
+        expect(number15).toBe("FIZZBUZZ");
+    });
+    test("6) Test for new service applyValidationInNumber equal to 3", () => {
+        const number3 = FizzBuzzService.applyValidationInNumber(3);
+        expect(number3).toBe("FIZZ");
+    });
+    test("6) Test for new service applyValidationInNumber equal to 3", () => {
+        const number5 = FizzBuzzService.applyValidationInNumber(5);
+        expect(number5).toBe("BUZZ");
+    });
+    test("7) Test for new service applyValidationInNumber with number not divisible by 3 or 5", () => {
+        const number1 = FizzBuzzService.applyValidationInNumber(1);
+        expect(number1).toBe(1);
+    });
 });
